62764c682e3990d52bf36171f80c793d125adc31
Ariel Zinger <arielzin33@gmail.com>
2026-08-21 11:35:53 +0300

    Delete Week16 directory

--- Week16/day7 (Projects)/Daily Challenge Tic-Tac-Toe Game/Header.jsx
+++
@@ -1,5 +0,0 @@
-function Header() {
-  return <h1 className="header">React Tic Tac Toe</h1>;
-}
-
-export default Header;
--- Week16/day7 (Projects)/Mini-Project  Build a Search Book App/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header className="app-header">
-      <h1>📚 Book Search</h1>
-    </header>
-  );
-}
-
-export default Header;
